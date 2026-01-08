5808ee23fc67705923659b2f9401bf734944d3c8
co22370 <co22370@ccet.ac.in>
2026-01-09 01:28:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -84,5 +84,5 @@ def chat():
         return jsonify({"reply": f"Backend error: {str(e)}"}), 500
 
 if __name__ == "__main__":
-    webbrowser.open("http://127.0.0.1:5000")
-    app.run(port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
